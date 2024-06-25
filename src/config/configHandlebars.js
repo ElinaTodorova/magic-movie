69e9ae876afc644be6f69c069326c00eea9b3825
ElinaTodorova <elina.todorova93@gmail.com>
2024-06-25 10:04:51 +0200

    Add movie controller for method get

--- src/config/configHandlebars.js
+++ src/config/configHandlebars.js
@@ -2,11 +2,11 @@ const handlebars = require("express-handlebars");
 const path = require("path");
 
 function configHandlebars(app) {
-    app.engine("hbs", handlebars.engine({
-        extname: "hbs"
+    app.engine('hbs', handlebars.engine({
+        extname: 'hbs'
     }));
-    app.set("view engine", "hbs");
-    app.set("views", path.resolve('src/views'))
+    app.set('view engine', 'hbs');
+    app.set('views', path.resolve('src/views'))
 }
 
 module.exports = configHandlebars;
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 
-const homeController = require("./controllers/homeController.js")
+const homeController = require("./controllers/homeController.js");
+const movieController = require("./controllers/movieController.js");
 
 router.use(homeController);
+router.use(movieController);
 
 module.exports = router;
