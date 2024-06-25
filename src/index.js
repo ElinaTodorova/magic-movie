38723ef1538fd7e39616b558fd2a49b4a597cbb2
ElinaTodorova <elina.todorova93@gmail.com>
2024-06-25 09:29:12 +0200

    Create first route

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const routes = require('./routes.js')
 
 const app = express();
 const port = 5000;
+
+app.use(routes)
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`)
