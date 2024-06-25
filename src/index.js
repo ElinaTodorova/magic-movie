6174c4aadac26ad93bfada7d3e1b0ac3647816c7
ElinaTodorova <elina.todorova93@gmail.com>
2024-06-25 09:43:20 +0200

    Separate configs
    Config express
    Config handlebars

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 const express = require("express");
-const routes = require('./routes.js')
+const routes = require('./routes.js');
+const configExpress = require("./config/configExpress.js");
+const configHandlebars = require("./config/configHandlebars.js")
 
 const app = express();
 const port = 5000;
+
+configExpress(app);
+configHandlebars(app);
 
 app.use(routes)
 
