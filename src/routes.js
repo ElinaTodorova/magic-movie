34e4b711d57802df2ca535e949fe81d527904c90
ElinaTodorova <elina.todorova93@gmail.com>
2024-07-01 11:24:05 +0200

    Add create cast form

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,11 @@ const router = require("express").Router();
 
 const homeController = require("./controllers/homeController.js");
 const movieController = require("./controllers/movieController.js");
+const castController = require("./controllers/castControlles.js");
 
 router.use(homeController);
 router.use(movieController);
+router.use(castController);
 
 router.get('*', (req, res) => {
     res.redirect('/404');
